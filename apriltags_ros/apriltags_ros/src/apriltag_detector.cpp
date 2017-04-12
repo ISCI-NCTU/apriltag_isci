c12bf25cd6b1662fb98726bb9a2e3428914d689e
s880367 <s880367@gmail.com>
2017-04-12 22:29:36 +0800

    Change the tf broadcaster order
    In line 243. I change the child_frame and parent_frame
    so that it would be easiler for multiple camera detect the same tag
    which is the same parent_frame.

--- apriltags_ros/apriltags_ros/src/apriltag_detector.cpp
+++ apriltags_ros/apriltags_ros/src/apriltag_detector.cpp
@@ -14,235 +14,271 @@
 #include <AprilTags/Tag36h11.h>
 #include <XmlRpcException.h>
 #include <iostream>
-#include <fstream>  
+#include <fstream>
 
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-namespace apriltags_ros{
+namespace apriltags_ros
+{
 
 ofstream data("/home/astrobotics/Desktop/data.txt");
 
-AprilTagDetector::AprilTagDetector(ros::NodeHandle& nh, ros::NodeHandle& pnh): it_(nh){
-  XmlRpc::XmlRpcValue april_tag_descriptions;
-  if(!pnh.getParam("tag_descriptions", april_tag_descriptions)){
-    ROS_WARN("No april tags specified");
-  }
-  else{
-    try{
-      descriptions_ = parse_tag_descriptions(april_tag_descriptions);
-    } catch(XmlRpc::XmlRpcException e){
-      ROS_ERROR_STREAM("Error loading tag descriptions: "<<e.getMessage());
+AprilTagDetector::AprilTagDetector(ros::NodeHandle& nh, ros::NodeHandle& pnh): it_(nh)
+{
+    XmlRpc::XmlRpcValue april_tag_descriptions;
+    string camera_name;
+    
+    if (!pnh.getParam("camera_name", camera_name))
+    {
+        ROS_WARN("No camera name specified");    
     }
-  }
 
-  if(!pnh.getParam("sensor_frame_id", sensor_frame_id_)){
-    sensor_frame_id_ = "";
-  }
+    if (!pnh.getParam("tag_descriptions", april_tag_descriptions))
+    {
+        ROS_WARN("No april tags specified");
+    }
+    else
+    {
+        try
+        {
+            descriptions_ = parse_tag_descriptions(april_tag_descriptions);
+        }
+        catch (XmlRpc::XmlRpcException e)
+        {
+            ROS_ERROR_STREAM("Error loading tag descriptions: " << e.getMessage());
+        }
+    }
 
-  std::string tag_family;
-  pnh.param<std::string>("tag_family", tag_family, "36h11");
+    if (!pnh.getParam("sensor_frame_id", sensor_frame_id_))
+    {
+        sensor_frame_id_ = "";
+    }
 
-  pnh.param<bool>("projected_optics", projected_optics_, false);
+    std::string tag_family;
+    pnh.param<std::string>("tag_family", tag_family, "36h11");
 
-  const AprilTags::TagCodes* tag_codes;
-  if(tag_family == "16h5"){
-    tag_codes = &AprilTags::tagCodes16h5;
-  }
-  else if(tag_family == "25h7"){
-    tag_codes = &AprilTags::tagCodes25h7;
-  }
-  else if(tag_family == "25h9"){
-    tag_codes = &AprilTags::tagCodes25h9;
-  }
-  else if(tag_family == "36h9"){
-    tag_codes = &AprilTags::tagCodes36h9;
-  }
-  else if(tag_family == "36h11"){
-    tag_codes = &AprilTags::tagCodes36h11;
-  }
-  else{
-    ROS_WARN("Invalid tag family specified; defaulting to 36h11");
-    tag_codes = &AprilTags::tagCodes36h11;
-  }
+    pnh.param<bool>("projected_optics", projected_optics_, false);
 
-  tag_detector_= boost::shared_ptr<AprilTags::TagDetector>(new AprilTags::TagDetector(*tag_codes));
-  image_sub_ = it_.subscribeCamera("image_rect", 1, &AprilTagDetector::imageCb, this);
-  image_pub_ = it_.advertise("tag_detections_image", 1);
-  detections_pub_ = nh.advertise<AprilTagDetectionArray>("tag_detections", 1);
-  pose_pub_ = nh.advertise<geometry_msgs::PoseArray>("tag_detections_pose", 1);
-  cam_pose_=nh.advertise<MetaPose>("cam_pose",1);
+    const AprilTags::TagCodes* tag_codes;
+    if (tag_family == "16h5")
+    {
+        tag_codes = &AprilTags::tagCodes16h5;
+    }
+    else if (tag_family == "25h7")
+    {
+        tag_codes = &AprilTags::tagCodes25h7;
+    }
+    else if (tag_family == "25h9")
+    {
+        tag_codes = &AprilTags::tagCodes25h9;
+    }
+    else if (tag_family == "36h9")
+    {
+        tag_codes = &AprilTags::tagCodes36h9;
+    }
+    else if (tag_family == "36h11")
+    {
+        tag_codes = &AprilTags::tagCodes36h11;
+    }
+    else
+    {
+        ROS_WARN("Invalid tag family specified; defaulting to 36h11");
+        tag_codes = &AprilTags::tagCodes36h11;
+    }
+
+    tag_detector_ = boost::shared_ptr<AprilTags::TagDetector>(new AprilTags::TagDetector(*tag_codes));
+    image_sub_ = it_.subscribeCamera("image_rect", 1, &AprilTagDetector::imageCb, this);
+    image_pub_ = it_.advertise(camera_name+"/tag_detections_image", 1);
+    detections_pub_ = nh.advertise<AprilTagDetectionArray>(camera_name+"/tag_detections", 1);
+    pose_pub_ = nh.advertise<geometry_msgs::PoseArray>(camera_name+"/tag_detections_pose", 1);
+    cam_pose_ = nh.advertise<MetaPose>(camera_name+"/cam_pose", 1);
 }
-AprilTagDetector::~AprilTagDetector(){
-  image_sub_.shutdown();
+AprilTagDetector::~AprilTagDetector()
+{
+    image_sub_.shutdown();
 }
 
-void AprilTagDetector::imageCb(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info){
-  cv_bridge::CvImagePtr cv_ptr;
-  try{
-    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-  }
-  catch (cv_bridge::Exception& e){
-    ROS_ERROR("cv_bridge exception: %s", e.what());
-    return;
-  }
+void AprilTagDetector::imageCb(const sensor_msgs::ImageConstPtr& msg, const sensor_msgs::CameraInfoConstPtr& cam_info)
+{
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
 
 
     data.open("/home/astrobotics/Desktop/data.txt", fstream::app);
 
 
-  cv::Mat gray;
-  cv::cvtColor(cv_ptr->image, gray, CV_BGR2GRAY);
-  std::vector<AprilTags::TagDetection>	detections = tag_detector_->extractTags(gray);
-  ROS_DEBUG("%d tag detected", (int)detections.size());
+    cv::Mat gray;
+    cv::cvtColor(cv_ptr->image, gray, CV_BGR2GRAY);
+    std::vector<AprilTags::TagDetection>  detections = tag_detector_->extractTags(gray);
+    ROS_DEBUG("%d tag detected", (int)detections.size());
 
-  double fx;
-  double fy;
-  double px;
-  double py;
-  if (projected_optics_) {
-    // use projected focal length and principal point
-    // these are the correct values
-    fx = cam_info->P[0];
-    fy = cam_info->P[5];
-    px = cam_info->P[2];
-    py = cam_info->P[6];
-  } else {
-    // use camera intrinsic focal length and principal point
-    // for backwards compatability
-    fx = cam_info->K[0];
-    fy = cam_info->K[4];
-    px = cam_info->K[2];
-    py = cam_info->K[5];
-  }
-
-
-
-  if(!sensor_frame_id_.empty())
-    cv_ptr->header.frame_id = sensor_frame_id_;
-
-  AprilTagDetectionArray tag_detection_array;
-  geometry_msgs::PoseArray tag_pose_array;
-  tag_pose_array.header = cv_ptr->header;
-  geometry_msgs::PoseStamped camera_position_in_tag_frame;
-
-  BOOST_FOREACH(AprilTags::TagDetection detection, detections){
-
-    std::map<int, AprilTagDescription>::const_iterator description_itr = descriptions_.find(detection.id);
-    if(description_itr == descriptions_.end()){
-      ROS_WARN_THROTTLE(10.0, "Found tag: %d, but no description was found for it", detection.id);
-      continue;
+    double fx;
+    double fy;
+    double px;
+    double py;
+    if (projected_optics_)
+    {
+        // use projected focal length and principal point
+        // these are the correct values
+        fx = cam_info->P[0];
+        fy = cam_info->P[5];
+        px = cam_info->P[2];
+        py = cam_info->P[6];
     }
-    AprilTagDescription description = description_itr->second;
-    double tag_size = description.size();
-
-    detection.draw(cv_ptr->image);
-
-    std::pair<float,float> temp;
-    temp.first=0;
-    temp.second=0;
-    std::pair<float,float> * centerxy = &temp;
-
-    Eigen::Matrix4d transform = detection.getRelativeTransform(tag_size, fx, fy, px, py, centerxy);
-    Eigen::Matrix3d rot = transform.block(0, 0, 3, 3);
-    Eigen::Quaternion<double> rot_quaternion = Eigen::Quaternion<double>(rot);
-
-    geometry_msgs::PoseStamped tag_pose;
-    MetaPose Mpose;
-    
-    tag_pose.pose.position.x = transform(0, 3);
-    tag_pose.pose.position.y = transform(1, 3);
-    tag_pose.pose.position.z = transform(2, 3);
-
-    data << "<" << centerxy->first << "," << centerxy->second << ">\n";
-    
-    data.close();
-
-    tag_pose.pose.orientation.x = rot_quaternion.x();
-    tag_pose.pose.orientation.y = rot_quaternion.y();
-    tag_pose.pose.orientation.z = rot_quaternion.z();
-    tag_pose.pose.orientation.w = rot_quaternion.w();
-    tag_pose.header = cv_ptr->header;
-
-    float xsqr = tag_pose.pose.orientation.x*tag_pose.pose.orientation.x;
-
-    //roll(around the y)
-    float t0 = +2.0f * (tag_pose.pose.orientation.w * tag_pose.pose.orientation.z + tag_pose.pose.orientation.x * tag_pose.pose.orientation.y);
-    float t1 = +1.0f - 2.0f * (tag_pose.pose.orientation.z * tag_pose.pose.orientation.z + xsqr);
-    Mpose.pose.orientation.y = atan2(t0,t1);
-
-    //pitch(around the x)
-    float t2 = +2.0f * (tag_pose.pose.orientation.w * tag_pose.pose.orientation.x - tag_pose.pose.orientation.y * tag_pose.pose.orientation.z);
-    t2 = t2 > 1.0f ? 1.0f : t2;
-    t2 = t2 < -1.0f ? -1.0f : t2;
-    Mpose.pose.orientation.x = asin(t2);
-
-    //yaw (around the z)
-    float t3 = +2.0f * (tag_pose.pose.orientation.w * tag_pose.pose.orientation.y + tag_pose.pose.orientation.z * tag_pose.pose.orientation.x);
-    float t4 = +1.0f - 2.0f * (xsqr + tag_pose.pose.orientation.y * tag_pose.pose.orientation.y);
-    Mpose.pose.orientation.z = atan2(t3,t4);
-    if(Mpose.pose.orientation.z < 0){
-      Mpose.pose.orientation.z += (2*M_PI);
+    else
+    {
+        // use camera intrinsic focal length and principal point
+        // for backwards compatability
+        fx = cam_info->K[0];
+        fy = cam_info->K[4];
+        px = cam_info->K[2];
+        py = cam_info->K[5];
     }
 
-    Mpose.pose.orientation.w = 1.00;
-    Mpose.detected = true;
 
-    Mpose.pose.position.y = sqrt(tag_pose.pose.position.x*tag_pose.pose.position.x + tag_pose.pose.position.z*tag_pose.pose.position.z)
-      *cos(Mpose.pose.orientation.y - M_PI - atan(tag_pose.pose.position.x/tag_pose.pose.position.z));
-    Mpose.pose.position.x = sqrt(tag_pose.pose.position.x*tag_pose.pose.position.x + tag_pose.pose.position.z*tag_pose.pose.position.z)
-      *sin(Mpose.pose.orientation.y - M_PI - atan(tag_pose.pose.position.x/tag_pose.pose.position.z));
-    Mpose.pose.position.z = tag_pose.pose.position.y;
 
-    Mpose.px=centerxy->first;
-    Mpose.py=centerxy->second;
+    if (!sensor_frame_id_.empty())
+        cv_ptr->header.frame_id = sensor_frame_id_;
 
-	  cam_pose_.publish(Mpose);
+    AprilTagDetectionArray tag_detection_array;
+    geometry_msgs::PoseArray tag_pose_array;
+    tag_pose_array.header = cv_ptr->header;
+    geometry_msgs::PoseStamped camera_position_in_tag_frame;
 
-    AprilTagDetection tag_detection;
-    tag_detection.pose = tag_pose;
-    tag_detection.id = detection.id;
-    tag_detection.size = tag_size;
-    tag_detection_array.detections.push_back(tag_detection);
-    tag_pose_array.poses.push_back(tag_pose.pose);
+    BOOST_FOREACH(AprilTags::TagDetection detection, detections)
+    {
 
-    tf::Stamped<tf::Transform> tag_transform;
-    tf::poseStampedMsgToTF(tag_pose, tag_transform);
-    tf_pub_.sendTransform(tf::StampedTransform(tag_transform, tag_transform.stamp_, tag_transform.frame_id_, description.frame_name()));
-  }
-  detections_pub_.publish(tag_detection_array);
-  pose_pub_.publish(tag_pose_array);
-  image_pub_.publish(cv_ptr->toImageMsg());
+        std::map<int, AprilTagDescription>::const_iterator description_itr = descriptions_.find(detection.id);
+        if (description_itr == descriptions_.end())
+        {
+            ROS_WARN_THROTTLE(10.0, "Found tag: %d, but no description was found for it", detection.id);
+            continue;
+        }
+        AprilTagDescription description = description_itr->second;
+        double tag_size = description.size();
+
+        detection.draw(cv_ptr->image);
+
+        std::pair<float, float> temp;
+        temp.first = 0;
+        temp.second = 0;
+        std::pair<float, float> * centerxy = &temp;
+
+        Eigen::Matrix4d transform = detection.getRelativeTransform(tag_size, fx, fy, px, py, centerxy);
+        Eigen::Matrix3d rot = transform.block(0, 0, 3, 3);
+        Eigen::Quaternion<double> rot_quaternion = Eigen::Quaternion<double>(rot);
+
+        geometry_msgs::PoseStamped tag_pose;
+        MetaPose Mpose;
+
+        tag_pose.pose.position.x = transform(0, 3);
+        tag_pose.pose.position.y = transform(1, 3);
+        tag_pose.pose.position.z = transform(2, 3);
+
+        data << "<" << centerxy->first << "," << centerxy->second << ">\n";
+
+        data.close();
+
+        tag_pose.pose.orientation.x = rot_quaternion.x();
+        tag_pose.pose.orientation.y = rot_quaternion.y();
+        tag_pose.pose.orientation.z = rot_quaternion.z();
+        tag_pose.pose.orientation.w = rot_quaternion.w();
+        tag_pose.header = cv_ptr->header;
+
+        float xsqr = tag_pose.pose.orientation.x * tag_pose.pose.orientation.x;
+
+        //roll(around the y)
+        float t0 = +2.0f * (tag_pose.pose.orientation.w * tag_pose.pose.orientation.z + tag_pose.pose.orientation.x * tag_pose.pose.orientation.y);
+        float t1 = +1.0f - 2.0f * (tag_pose.pose.orientation.z * tag_pose.pose.orientation.z + xsqr);
+        Mpose.pose.orientation.y = atan2(t0, t1);
+
+        //pitch(around the x)
+        float t2 = +2.0f * (tag_pose.pose.orientation.w * tag_pose.pose.orientation.x - tag_pose.pose.orientation.y * tag_pose.pose.orientation.z);
+        t2 = t2 > 1.0f ? 1.0f : t2;
+        t2 = t2 < -1.0f ? -1.0f : t2;
+        Mpose.pose.orientation.x = asin(t2);
+
+        //yaw (around the z)
+        float t3 = +2.0f * (tag_pose.pose.orientation.w * tag_pose.pose.orientation.y + tag_pose.pose.orientation.z * tag_pose.pose.orientation.x);
+        float t4 = +1.0f - 2.0f * (xsqr + tag_pose.pose.orientation.y * tag_pose.pose.orientation.y);
+        Mpose.pose.orientation.z = atan2(t3, t4);
+        if (Mpose.pose.orientation.z < 0)
+        {
+            Mpose.pose.orientation.z += (2 * M_PI);
+        }
+
+        Mpose.pose.orientation.w = 1.00;
+        Mpose.detected = true;
+
+        Mpose.pose.position.y = sqrt(tag_pose.pose.position.x * tag_pose.pose.position.x + tag_pose.pose.position.z * tag_pose.pose.position.z)
+                                * cos(Mpose.pose.orientation.y - M_PI - atan(tag_pose.pose.position.x / tag_pose.pose.position.z));
+        Mpose.pose.position.x = sqrt(tag_pose.pose.position.x * tag_pose.pose.position.x + tag_pose.pose.position.z * tag_pose.pose.position.z)
+                                * sin(Mpose.pose.orientation.y - M_PI - atan(tag_pose.pose.position.x / tag_pose.pose.position.z));
+        Mpose.pose.position.z = tag_pose.pose.position.y;
+
+        Mpose.px = centerxy->first;
+        Mpose.py = centerxy->second;
+
+        cam_pose_.publish(Mpose);
+
+        AprilTagDetection tag_detection;
+        tag_detection.pose = tag_pose;
+        tag_detection.id = detection.id;
+        tag_detection.size = tag_size;
+        tag_detection_array.detections.push_back(tag_detection);
+        tag_pose_array.poses.push_back(tag_pose.pose);
+
+        tf::Stamped<tf::Transform> tag_transform;
+        tf::poseStampedMsgToTF(tag_pose, tag_transform);
+       // tf_pub_.sendTransform(tf::StampedTransform(tag_transform, tag_transform.stamp_, tag_transform.frame_id_, description.frame_name()));
+        tf_pub_.sendTransform(tf::StampedTransform(tag_transform, tag_transform.stamp_, description.frame_name(), tag_transform.frame_id_));
+    }
+    detections_pub_.publish(tag_detection_array);
+    pose_pub_.publish(tag_pose_array);
+    image_pub_.publish(cv_ptr->toImageMsg());
 }
 
 
-std::map<int, AprilTagDescription> AprilTagDetector::parse_tag_descriptions(XmlRpc::XmlRpcValue& tag_descriptions){
-  std::map<int, AprilTagDescription> descriptions;
-  ROS_ASSERT(tag_descriptions.getType() == XmlRpc::XmlRpcValue::TypeArray);
-  for (int32_t i = 0; i < tag_descriptions.size(); ++i) {
-    XmlRpc::XmlRpcValue& tag_description = tag_descriptions[i];
-    ROS_ASSERT(tag_description.getType() == XmlRpc::XmlRpcValue::TypeStruct);
-    ROS_ASSERT(tag_description["id"].getType() == XmlRpc::XmlRpcValue::TypeInt);
-    ROS_ASSERT(tag_description["size"].getType() == XmlRpc::XmlRpcValue::TypeDouble);
+std::map<int, AprilTagDescription> AprilTagDetector::parse_tag_descriptions(XmlRpc::XmlRpcValue& tag_descriptions)
+{
+    std::map<int, AprilTagDescription> descriptions;
+    ROS_ASSERT(tag_descriptions.getType() == XmlRpc::XmlRpcValue::TypeArray);
+    for (int32_t i = 0; i < tag_descriptions.size(); ++i)
+    {
+        XmlRpc::XmlRpcValue& tag_description = tag_descriptions[i];
+        ROS_ASSERT(tag_description.getType() == XmlRpc::XmlRpcValue::TypeStruct);
+        ROS_ASSERT(tag_description["id"].getType() == XmlRpc::XmlRpcValue::TypeInt);
+        ROS_ASSERT(tag_description["size"].getType() == XmlRpc::XmlRpcValue::TypeDouble);
 
-    int id = (int)tag_description["id"];
-    double size = (double)tag_description["size"];
+        int id = (int)tag_description["id"];
+        double size = (double)tag_description["size"];
 
-    std::string frame_name;
-    if(tag_description.hasMember("frame_id")){
-      ROS_ASSERT(tag_description["frame_id"].getType() == XmlRpc::XmlRpcValue::TypeString);
-      frame_name = (std::string)tag_description["frame_id"];
+        std::string frame_name;
+        if (tag_description.hasMember("frame_id"))
+        {
+            ROS_ASSERT(tag_description["frame_id"].getType() == XmlRpc::XmlRpcValue::TypeString);
+            frame_name = (std::string)tag_description["frame_id"];
+        }
+        else
+        {
+            std::stringstream frame_name_stream;
+            frame_name_stream << "tag_" << id;
+            frame_name = frame_name_stream.str();
+        }
+        AprilTagDescription description(id, size, frame_name);
+        ROS_INFO_STREAM("Loaded tag config: " << id << ", size: " << size << ", frame_name: " << frame_name);
+        descriptions.insert(std::make_pair(id, description));
     }
-    else{
-      std::stringstream frame_name_stream;
-      frame_name_stream << "tag_" << id;
-      frame_name = frame_name_stream.str();
-    }
-    AprilTagDescription description(id, size, frame_name);
-    ROS_INFO_STREAM("Loaded tag config: "<<id<<", size: "<<size<<", frame_name: "<<frame_name);
-    descriptions.insert(std::make_pair(id, description));
-  }
-  return descriptions;
+    return descriptions;
 }
 
 
